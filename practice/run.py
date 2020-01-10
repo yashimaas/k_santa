698453549735ed88fa193f1f767ea81a23bed8d3
tks3141 <28818747+tks3141@users.noreply.github.com>
2020-01-10 22:46:37 +0900

    gurobi

--- practice/run.py
+++ practice/run.py
@@ -1,9 +1,10 @@
 from gurobipy import *
-m = read("santa_sm.lp")
+m = read("santa.lp")
+m.read("kernel_init.mst")
 m.setParam("NodefileStart", 2.0)
-setParam("MIPFocus", 1)
+setParam("MIPFocus", 0)
 
 m.optimize()
-m.write("s_s.lp")
-m.write("s_s.mst")
-m.write("s_s.sol")
+m.write("santa_r.lp")
+m.write("santa_r.mst")
+m.write("santa_r.sol")
